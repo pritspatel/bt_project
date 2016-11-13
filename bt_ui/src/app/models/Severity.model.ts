da449e0289f75411a44b1e5a847b75a01e0ebc60
Pritesh Patel <3ppatel@gmail.com>
2016-11-13 16:25:52 -0500

    adding severity crud ui

--- bt_ui/src/app/models/Severity.model.ts
+++ bt_ui/src/app/models/Severity.model.ts
@@ -2,5 +2,5 @@ export class Severity {
     private id: number;
     private createdAt: any;
     private updatedAt: any;
-    constructor(public code: string, public name: string, public rank: number) { }
+    constructor(public code?: string, public name?: string, public rank?: number) { }
 }
--- bt_ui/src/app/severity-list/severity-list.component.ts
+++ bt_ui/src/app/severity-list/severity-list.component.ts
@@ -1,3 +1,5 @@
+import { SeverityService } from './../services/Severity.service';
+import { Severity } from './../models/Severity.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,95 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeverityListComponent implements OnInit {
 
-  constructor() { }
+   newProject = false;
+  severity: Severity = new Severity();
+  displayDialog: boolean = false;
+  selectedProject: Severity;
+  severityList: Severity[];
 
+  constructor(private _severityService: SeverityService) { }
+
+  /**
+   * Lifecycle hook for on init
+   */
   ngOnInit() {
+    console.log('Firing on init');
+    this._severityService.getSeverities()
+      .subscribe(
+      data => this.severityList = data
+      );
   }
+
+
+  /**
+   * Sets certain properties to show dialog.
+   */
+  showDialogToAdd() {
+    this.newProject = true;
+    this.severity = new Severity();
+    this.displayDialog = true;
+  }
+
+
+
+  /**
+   * Called when 'Save' button is clicked
+   */
+  save(event) {
+    if (this.newProject) {
+      console.log(this.severity);
+      this._severityService.createSeverity(this.severity).subscribe(
+        data => this.severityList.push(data)
+      );
+    } else {
+
+      console.log('Edit ---' + this.severity);
+      this._severityService.updateSeverity(this.severity).subscribe(
+        data => this.severityList[this.findSelectedProjectIndex()] = data
+      );
+    }
+
+    this.severity = null;
+    this.displayDialog = false;
+  }
+
+  /**
+   * Method to find selected severity from severity array
+   */
+  findSelectedProjectIndex(): number {
+    console.log('Selected index : ' + this.severityList.indexOf(this.selectedProject));
+    return this.severityList.indexOf(this.selectedProject);
+  }
+
+  /**
+   * Method to delete the data
+   */
+  delete() {
+    this.severity = this.severityList[this.findSelectedProjectIndex()];
+    this._severityService.deleteSeverity(this.severity).subscribe(
+      data => this.severity = data
+    );
+    this.severityList.splice(this.findSelectedProjectIndex(), 1);
+    this.severity = null;
+    this.displayDialog = false;
+  }
+
+  /**
+   * returns selected data.
+   */
+  onRowSelect(event) {
+    this.newProject = false;
+    this.severity = this.cloneProject(event.data);
+    this.displayDialog = true;
+  }
+
+  cloneProject(p: Severity): Severity {
+    let severity = new Severity();
+    for (let prop in p) {
+      severity[prop] = p[prop];
+    }
+    return severity;
+  }
+
 
 }
